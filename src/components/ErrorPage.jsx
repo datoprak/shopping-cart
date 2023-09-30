c0f64f627a072dfedc664b0eecdfcd9b87bc32c4
datoprak <106121887+datoprak@users.noreply.github.com>
2023-09-30 17:40:46 +0300

    Add useRouteError to ErrorPage

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,6 +1,20 @@
+import { Link, useRouteError } from "react-router-dom";
+
 const ErrorPage = () => {
+  const error = useRouteError();
+  console.error(error);
+
   return (
-    <div>ErrorPage</div>
-  )
-}
-export default ErrorPage
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occured.</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+      <button>
+        <Link to={"/"}>Return to Homepage</Link>
+      </button>
+    </div>
+  );
+};
+export default ErrorPage;

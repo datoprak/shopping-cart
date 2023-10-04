d57fd1fb548fa2ec383b403fcb653b59b64168d4
datoprak <106121887+datoprak@users.noreply.github.com>
2023-10-04 23:21:53 +0300

    Create Home styling and change logo font

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,35 @@
+import "../styles/Home.css";
+import image from "../assets/headerImg.jpg";
+
 const Home = () => {
   return (
-    <>
+    <div className="home">
       <section className="left-section">
-        <h1 className="header">Welcome to Shop</h1>
-        <p>
+        <h1 className="hero-header">
+          Welcome to <span>The Shop</span>
+        </h1>
+        <p className="hero-text">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
           fermentum pellentesque volutpat. Fusce convallis posuere suscipit.
           Praesent non sagittis risus. Nam viverra elit metus, id mattis sem
           congue sit amet. Nulla porttitor ipsum vitae odio sodales, vel feugiat
           neque scelerisque.
         </p>
+        <p className="hero-text">
+          Nam viverra elit metus, id mattis sem congue sit amet. Nulla porttitor
+          ipsum vitae odio sodales, vel feugiat neque scelerisque.
+        </p>
+        <p className="hero-text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+          fermentum pellentesque volutpat. Fusce convallis posuere suscipit.
+          Praesent non sagittis risus.
+        </p>
       </section>
       <section className="right-section">
-        <div>DUMMY IMAGE</div>
+        <img src={image} alt="header hero image with coats" />
       </section>
-    </>
-  )
-}
-export default Home
+    </div>
+  );
+};
+
+export default Home;

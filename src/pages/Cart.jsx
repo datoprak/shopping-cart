891b4dce38960023ae2a31606930e49afcecfc45
datoprak <106121887+datoprak@users.noreply.github.com>
2023-10-05 20:35:34 +0300

    Create cart page styling

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,4 +1,5 @@
 import { Link, useOutletContext } from "react-router-dom";
+import "../styles/Cart.css";
 
 const Cart = () => {
   const { cartState, cartTotal, setCartNumber } = useOutletContext();
@@ -10,30 +11,43 @@ const Cart = () => {
   };
 
   return (
-    <>
+    <div className="cart-page">
       {cart.length > 0 ? (
         <>
+          <button className="empty-cart-btn" onClick={handleEmpty}>
+            Empty the cart
+          </button>
+          <div className="cart-table">
+            <div className="cart-table-img">Image</div>
+            <div>Title</div>
+            <div className="cart-table-quantity">Quantity</div>
+            <div className="cart-table-price">Total Price</div>
+          </div>
           {cart.map(product => {
             return (
-              <div className="cart-container" key={product.id}>
-                <img src={product.image} alt={product.title} />
-                <div className="item-title">{product.title}</div>
-                <div className="item-quantity">{product.quantity}</div>
-                <div className="item-total-price">${product.totalPrice}</div>
+              <div className="cart-card" key={product.id}>
+                <Link to={`/products/${product.id}`}>
+                  <img src={product.image} alt={product.title} />
+                  <div className="item-title">{product.title}</div>
+                  <div className="item-quantity">{product.quantity}</div>
+                  <div className="item-total-price">${product.totalPrice}</div>
+                </Link>
               </div>
             );
           })}
-
-          <div className="cart-total-price">Total Price: ${cartTotal}</div>
-          <button onClick={handleEmpty}>Empty the cart</button>
+          <div className="cart-total-price">
+            Cart&apos;s Total Price: <span>${cartTotal}</span>
+          </div>
           <Link to="/payment">
-            <button onClick={handleEmpty}>Checkout and Pay</button>
+            <button className="payment-btn" onClick={handleEmpty}>
+              Checkout and Pay
+            </button>
           </Link>
         </>
       ) : (
-        <div className="empty-card">Your cart is empty.</div>
+        <div className="empty-cart">Your cart is empty.</div>
       )}
-    </>
+    </div>
   );
 };
 export default Cart;
